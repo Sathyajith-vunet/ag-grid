1c543f9e72e61dbffc1a054ad1434abe7004531d
Sathyajith-vunet <sathyajith@vunetsystems.com>
2021-06-06 02:30:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import "./App.css";
 
-function App() {
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+import fetchRowData from "./rowData";
+
+const App = () => {
+
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    setRowData(fetchRowData());
+  }, []);
+
+  const renderRows = () => {
+    if (rowData.length > 0) {
+      return (
+        rowData[0] &&
+        Object.keys(rowData[0]).map((key) => {
+          return (
+            <AgGridColumn
+              key={key}
+              field={key}
+              sortable={true}
+              filter={true}
+            ></AgGridColumn>
+          );
+        })
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">Event Console</div>
+      <div className="ag-theme-material ag-grid-table">
+        <AgGridReact rowData={rowData}>
+          {renderRows()}
+        </AgGridReact>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
